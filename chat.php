5325e3dcdac6a577db12a43294179bbfc62749e2
xny2115 <83159645+xny2115@users.noreply.github.com>
2022-02-06 00:00:00 +0100

    Add files via upload

---
+++ chat.php
@@ -0,0 +1,195 @@
+
+<html xmlns="http://www.w3.org/1999/xhtml" lang="pl" xml:lang="pl"> 
+<html xmlns="http://www.w3.org/1999/xhtml" lang="pl" xml:lang="pl"> 
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="description" content="" />
+<meta name="keywords" content="" />
+<title>vehhty.tk</title>
+ 
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+
+</head>
+<style>
+
+/* Style the tab */
+.tab {
+  overflow: hidden;
+  border: 0px solid #ccc;
+  background-color: #424242;
+  
+}
+
+/* Style the buttons inside the tab */
+.tab button {
+  background-color: inherit;
+  float: left;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  margin-left:70px;
+  padding:5px;
+  transition: 0.3s;
+  font-size: 14px;
+  font-weight: bold;
+  font-family: arial;
+}
+
+/* Change background color of buttons on hover */
+.tab button:hover {
+  background-color: #424242;
+}
+
+/* Create an active/current tablink class */
+.tab button.active {
+  background-color:;
+}
+
+/* Style the tab content */
+.tabcontent {
+  display: none;
+
+}
+
+body {
+background: #1d1d1d;
+padding: 0px;
+margin: 0px;
+font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+a {
+text-decoration:none;
+color:#fff;  
+}
+
+h3 {
+float: left;
+margin-left: 10px;
+margin-top: 14px;
+color: steelblue;
+}
+
+h2 {
+padding-top: 10px;
+margin-top:10px;
+margin-left: 15px;
+float: left;
+}
+
+#searchInput {
+    -webkit-appearance: none;
+    font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
+    width: 180px;
+    margin-right:15px;
+    margin-top: 20px;
+    padding: 0 10px;
+    height: 24px;
+    font-size: 14px;
+    color: #666;
+    line-height: 24px;
+    border: 0;
+    border-radius: 50px;
+    position: relative;
+    z-index: 5;
+    -webkit-transition: .3s ease;
+    -moz-transition: .3s ease;
+    float:right;
+} 
+
+#belka {
+width: 283px;
+background: #292929;
+padding: 0px;
+margin: 0px;
+
+}
+
+#belka table{
+border-collapse: collapse;
+
+}
+
+.bramki {
+background-color: #292929;
+width: 265px;
+color: white;
+background: #292929;
+padding-top: 10px;
+padding-bottom: 10px;
+margin-bottom:10px;
+float:right;
+
+}
+</style>
+
+
+
+
+
+
+<div id="belka">
+
+<div class="tab">
+<button class="tablinks" onclick="openCity(event, '1')" id="defaultOpen">
+<a href="https://vehhty.tk/discord" target="_blank"><img src="https://i.imgur.com/pcIP4v1.png" width="125px" height="50px"></a></button>
+
+<!-- 
+<button class="tablinks" onclick="openCity(event, '2')"> 
+
+</button> -->
+
+
+</div>
+
+<div id="1" class="tabcontent">
+
+<script id="cid0020000283181485624" data-cfasync="false" async src="//st.chatango.com/js/gz/emb.js" style="width: 100%;height: 640px;">{"handle":"eseses1251252","arch":"js","styles":{"a":"000000","c":"e0e0e0","d":"ffffff","e":"ffffff","f":0,"g":"ffffff","h":"ffffff","i":0,"j":"ffffff","k":"ffffff","l":"000000","m":"000000","n":"ffffff","p":"9","q":"000000","t":0,"usricon":1.01,"sbc":"bbbbbb","surl":0}}</script>
+
+
+
+
+<center>
+<!--
+<script async="async" data-cfasync="false" src=""></script>
+<div id=""></div> -->
+
+<div data-nat="2063"></div>
+
+<div id="2" class="tabcontent">
+
+
+
+<div id="3" class="tabcontent">
+
+
+</div>
+
+
+
+
+
+
+
+</tbody></table></div>
+<script>
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+document.getElementById("1").style.display = 'block';
+document.getElementById("defaultOpen").className += " active";
+</script>
+
+</body>
+</html> 
